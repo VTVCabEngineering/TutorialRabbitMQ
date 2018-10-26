f1152afee82396fbf4ae0dd17165cadfddb11a28
quochung <hungfeck@gmail.com>
2018-10-26 17:23:51 +0700

    cap nhat source

---
+++ Sender/Sender/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tạo kết nối tới server RabbitMQ bao gồm tên server
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "yourUsername",
+                Password = "yourPassword",
+                VirtualHost = "yourVirtualHost"
+            };
+
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                // Khai báo queue có tên newMesage
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+                // Gửi thông báo tới exchange mặc định như mình có lưu ý trong bài viết, và gửi tới queue có tên là newMesage vừa được khai báo
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SenderJS/index.js
@@ -0,0 +1,10 @@
+amqp.connect('amqp://localhost', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var q = 'hello';
+
+    ch.assertQueue(q, {durable: false});
+    // Note: on Node 6 Buffer.from(msg) should be used
+    ch.sendToQueue(q, new Buffer('Hello World!'));
+    console.log(" [x] Sent 'Hello World!'");
+  });
+});
